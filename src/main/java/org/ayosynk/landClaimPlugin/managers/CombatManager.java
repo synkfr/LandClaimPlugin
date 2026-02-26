9efbcf2b42b47787d512afecfd48d3541bf362a4
synkfr <ayosynk0@gmail.com>
2026-02-26 23:51:12 +0530

    feat: integrate PvPManager to block commands during combat

--- src/main/java/org/ayosynk/landClaimPlugin/managers/CombatManager.java
+++ src/main/java/org/ayosynk/landClaimPlugin/managers/CombatManager.java
@@ -3,6 +3,7 @@ package org.ayosynk.landClaimPlugin.managers;
 import org.ayosynk.landClaimPlugin.LandClaimPlugin;
 import org.ayosynk.landClaimPlugin.hooks.combat.CombatHook;
 import org.ayosynk.landClaimPlugin.hooks.combat.DeluxeCombatHook;
+import org.ayosynk.landClaimPlugin.hooks.combat.PvPManagerHook;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -21,6 +22,10 @@ public class CombatManager {
         if (plugin.getServer().getPluginManager().getPlugin("DeluxeCombat") != null) {
             activeHooks.add(new DeluxeCombatHook());
             plugin.getLogger().info("Hooked into DeluxeCombat for combat tagging!");
+        }
+        if (plugin.getServer().getPluginManager().getPlugin("PvPManager") != null) {
+            activeHooks.add(new PvPManagerHook());
+            plugin.getLogger().info("Hooked into PvPManager for combat tagging!");
         }
     }
 
